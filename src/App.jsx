437540ed426714ff43c7ff82dd371bc9a6df26de
Yung Kim <yungkim10023@gmail.com>
2025-10-18 09:46:38 +1100

    switched around order of front-end and back-end and removed hover from back-end

--- src/App.jsx
+++ src/App.jsx
@@ -28,34 +28,6 @@ function App() {
         </div>
       <div className="projects">
         <FadeInSection fadeInThreshold={0.2} fadeOutThreshold={0.8}>
-          <div className="backend">
-            <h1>Back-end</h1>
-            <p>I am proficient in SQL, Java, Python, and using APIs (flask).</p>
-
-            <div className="backend_projects">
-              <div className="content-wrapper">
-                <h2>Virtual Wait System</h2>
-                <img src={kiosk} alt="Image of kiosk" className="kiosk-image"/>
-                <p>
-                  A fully functional kiosk screen and menu, eliminating the need for waiters to find a table and order food. <br />
-                  <strong>Primary Languages:</strong> SQLite, Python, Flask.
-                </p>
-              </div>
-            </div>
-            <div className="backend_projects">
-              <div className="content-wrapper">
-                <h2>Dungeon Crawler</h2>
-                <img src={dungeoncrawler} alt="Image of dungeoncrawler" className="dungeon-image"/>
-                <p>
-                  A 2D dungeon crawler game, with unique monster and weapon mechanics. <br />
-                  <strong>Primary Languages:</strong> Java.
-                </p>
-              </div>
-            </div>
-          </div>
-        </FadeInSection>
-
-        <FadeInSection fadeInThreshold={0.2} fadeOutThreshold={0.8}>
           <div className="frontend">
             <h1>Front-end</h1>
             <p>
@@ -84,8 +56,35 @@ function App() {
             </div>
           </div>
         </FadeInSection>
-      </div>
+      
+        <FadeInSection fadeInThreshold={0.2} fadeOutThreshold={0.8}>
+            <div className="backend">
+              <h1>Back-end</h1>
+              <p>I am proficient in SQL, Java, Python, and using APIs (flask).</p>
 
+              <div className="backend_projects">
+                <div className="content-wrapper-backend">
+                  <h2>Virtual Wait System</h2>
+                  <img src={kiosk} alt="Image of kiosk" className="kiosk-image"/>
+                  <p>
+                    A fully functional kiosk screen and menu, eliminating the need for waiters to find a table and order food. <br />
+                    <strong>Primary Languages:</strong> SQLite, Python, Flask.
+                  </p>
+                </div>
+              </div>
+              <div className="backend_projects">
+                <div className="content-wrapper-backend">
+                  <h2>Dungeon Crawler</h2>
+                  <img src={dungeoncrawler} alt="Image of dungeoncrawler" className="dungeon-image"/>
+                  <p>
+                    A 2D dungeon crawler game, with unique monster and weapon mechanics. <br />
+                    <strong>Primary Languages:</strong> Java.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </FadeInSection>
+        </div>
       {/* <FadeInSection fadeInThreshold={0.2} fadeOutThreshold={0.8}> */}
         <div className="links">
           <h1>Links</h1>
